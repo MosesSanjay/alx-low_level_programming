bb0f462a13634c75d76c61ffd7bac7f7586cb7d3
MosesSanjay <mosesajulor@gmail.com>
2022-09-14 11:44:38 -0700

    nested loops commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
-uth: Samson Ajulor
+/*
+
+ * File: 0-putchar.c
+
+ * Auth: Moses Ajulor
 
  *
- *
- *  */
+
+ */
 
 
 
@@ -10,40 +14,40 @@ uth: Samson Ajulor
 
 
 
- /**
-  *
-  *  * main - Prints "_putchar" followed by a new line.
-  *
-  *   *
-  *
-  *    * Return: Always 0.
-  *
-  *     */
+/**
 
- int main(void)
+ * main - Prints "_putchar" followed by a new line.
 
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
 {
+  
+  _putchar('_');
+  
+  _putchar('p');
+  
+  _putchar('u');
+  
+  _putchar('t');
+  
+  _putchar('c');
+  
+  _putchar('h');
+  
+  _putchar('a');
+  
+  _putchar('r');
+  
+  _putchar('\n');
+  
 
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-										_putchar('\n');
-
-
-
-											return (0);
-
+  
+  return (0);
+  
 }
